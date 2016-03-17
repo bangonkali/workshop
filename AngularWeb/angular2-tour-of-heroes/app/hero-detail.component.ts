a9c982cda14eeef9de9a9f8d34cf2bcd1b00fc67
gilmichael <gilmichael.regalado@emerson.com>
2016-03-17 19:20:59 +0800

    Capter 6. Done.

--- AngularWeb/angular2-tour-of-heroes/app/hero-detail.component.ts
+++ AngularWeb/angular2-tour-of-heroes/app/hero-detail.component.ts
@@ -1,20 +1,31 @@
 import {Component} from 'angular2/core';
+import {RouteParams} from 'angular2/router';
+import { HeroService } from './hero.service';
 import {Hero} from './hero';
 
 @Component({
     selector: 'my-hero-detail',
     inputs: ['hero'],
-    template: `
-<div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div><div>
-        <label>name: </label>
-        <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-</div>`
+    templateUrl: 'app/hero-detail.component.html',
+    styleUrls: ['app/hero-detail.component.css']
 })
 
 export class HeroDetailComponent {
+    constructor(
+        private _heroService: HeroService,
+        private _routeParams: RouteParams) {
+    }
+
+    ngOnInit() {
+        let id = +this._routeParams.get('id');
+        this._heroService.getHero(id)
+            .then(hero => this.hero = hero);
+    }
+
+    goBack() {
+        window.history.back();
+    }
+
     hero: Hero;
 }
 
